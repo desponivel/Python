e34db6c9cc53a81e6ce1babc9fdb31aef31339e9
quem sou eu? <167908922+desponivel@users.noreply.github.com>
2024-07-24 15:28:58 -0300

    Add files via upload
    pou

---
+++ licao luiz]/lição 2.py
@@ -0,0 +1,8 @@
+gasolina = float (input('qual o valor da gasolina? '))
+alcool = float (input('qual o valor do alcool? '))
+diferença = alcool, 15% gasolina
+
+if diferença< gasolina:
+    print('abastecer com alcool')
+else:
+    print('abastecer com gasolina')
